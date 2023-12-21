9842fecb8ac40f94abad142dd84aa8594cc08c14
Alex Bedulin <alexander.bedulin@gmail.com>
2023-12-21 10:27:04 +0100

    Initial Commit

---
+++ AppleTVTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleTVTestApp
+//
+//  Created by alex on 21.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
